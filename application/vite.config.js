1149c91fad1a8b31aec770ce351b5d6e5a009ef1
Mekan Saryyev <mekan@Mekans-MacBook-Pro.local>
2025-06-18 05:14:51 +0300

    deployed front

--- application/vite.config.js
+++ application/vite.config.js
@@ -3,10 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    allowedHosts: [
-      'be15-185-172-129-155.ngrok-free.app',
-      'localhost',
-    ],
-  },
+  build: {
+    outDir: '../'
+  }
 })
