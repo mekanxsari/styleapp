9c32501392eb3c855d8bd4f450bfe9aa302fd18e
Mekan Saryyev <mekan@Mekans-MacBook-Pro.local>
2025-06-19 11:57:34 +0300

    api

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,7 +5,7 @@ const authRouter = require('./auth');
 const checkSessionRouter = require('./check-session');
 const capculasRouter = require('./capculas');
 const outfitsRouter = require('./outfits');
-const capculaRouter = require('./capsula');
+const capculaRouter = require('./capcula');
 
 router.use('/auth', authRouter);
 router.use('/check-session', checkSessionRouter);
