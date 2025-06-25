e080e623c1ccb2c0aa4cbe5fc7481c22383a68b0
Mekan Saryyev <mekan@Mekans-MacBook-Pro.local>
2025-06-26 02:13:28 +0300

    minor changes

--- application/src/App.vue
+++ application/src/App.vue
@@ -30,7 +30,7 @@ function setupBackButton(path) {
       const referrer = document.referrer
       const loginWasLast = referrer.includes('/login')
 
-      if (window.history.length > 0 && !loginWasLast) {
+      if (!loginWasLast) {
         router.back()
       } else {
         router.push('/')
