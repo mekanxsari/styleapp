4d17e2c001ac80bd3ebe0f3ec51971a060e774d8
Mekan Saryyev <m.saryyev@innopolis.university>
2025-07-09 15:05:46 +0300

    fix

--- application/src/api.js
+++ application/src/api.js
@@ -1,5 +1,2 @@
-const DEV_API_URL = 'http://mekanxsari.ru:3000/api';
-const PROD_API_URL = '/api';
-
-export const API_URL = process.env.NODE_ENV === 'production' ? PROD_API_URL : DEV_API_URL;
+export const API_URL = '/api';
 export const SITE_URL = 'https://www.mekanxsari.ru';
